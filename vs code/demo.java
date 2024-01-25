1dcc053b1609a761bc8f608c0ac5f43d912b750a
henryzachary <149612405+henryzachary@users.noreply.github.com>
2024-01-25 12:30:05 -0500

    Add files via upload

---
+++ vs code/demo.java
@@ -0,0 +1,8 @@
+class HelloWorld {
+    public static void main(String args[]) throws Exception{
+       byte b = 120;
+       byte a = 120;
+       byte c = (byte) (a+b);
+       System.out.println(c);
+    }
+}
