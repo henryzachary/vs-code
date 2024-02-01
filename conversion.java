b8501bd69f7f455e9bbaf07024bac9e2924273d9
henryzachary <henryzachary@oakland.edu>
2024-02-01 13:58:31 -0500

    updated code

--- conversion.java
+++ conversion.java
@@ -6,66 +6,52 @@
 // km to miles  cm to inches kg to lbs 
 
 import java.util.Scanner;
-public class conversion {
+
+public class Conversion {
 
     public static void main(String[] args) {
-        double km, cm, m, mm;
-        int convNum, i;
-        String userinput = "";
+        Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Welcome to metric distance converter!");        
-        
-        try (Scanner in = new Scanner(System.in)) {
-            System.out.println("How many conversions would you like to make?");
-            
-            convNum = in.nextInt();
-            
-            for(i = 0; i<=convNum; i++){
-                System.out.println("Please input your query (km, m, cm, mm): ");
-   
-                userinput = in.nextLine();
-            
-                switch(userinput){
+        System.out.println("Welcome to the metric conversion program!");
 
+        while (true) {
+            System.out.println("\nEnter the conversion you would like to make (km, cm, mm, kg, exit): ");
+
+            
+            String choice = scanner.next();
+
+            switch (choice) {
                 case "km":
-                System.out.println("Enter the amount you would like to convert: ");
-                km = in.nextDouble();
-                System.out.println(km + " km is equal to " + (km*0.62137119) + " mi.");       
-                break;
-                
-                case "mm":
-                System.out.println("Enter the amount you would like to convert: ");
-                mm = in.nextDouble();
-                System.out.println(mm + " mm is equal to " + (mm*0.03937008) + " inches.");       
-                break;
-                
+                    System.out.print("Enter kilometers: ");
+                    double km = scanner.nextDouble();
+                    double miles = km * 0.62137119;
+                    System.out.println(km + " km is equal to " + miles + " miles.");
+                    break;
                 case "cm":
-                System.out.println("Enter the amount you would like to convert: ");
-                cm = in.nextDouble();
-                System.out.println(cm + " cm is equal to " + (cm*0.39370) + " in.");        
-                break;
-                
-                case "m":
-                System.out.println("Enter the amount you would like to convert: ");
-                m = in.nextDouble();
-                System.out.println(m + " m is equal to " + (m*3.2808399) + " feet."); 
-                break;
-                
-                case "exit":
-                in.close();
-                break;
-                
-     
-                
-
-            }}
+                    System.out.print("Enter centimeters: ");
+                    double cm = scanner.nextDouble();
+                    double inches = cm * 0.39370;
+                    System.out.println(cm + " cm is equal to " + inches + " inches.");
+                    break;
+                case "kg":
+                    System.out.print("Enter kilograms: ");
+                    double kg = scanner.nextDouble();
+                    double lbs = kg * 2.20462;
+                    System.out.println(kg + " kg is equal to " + lbs + " lbs.");
+                    break;
+                case "mm":
+                    System.out.print("Enter millimeters: ");
+                    double mm = scanner.nextDouble();
+                    double inch = mm * 0.03937008;
+                    System.out.println(mm + " mm is equal to " + inch + " inches.");
+                    break;
+                case "exit" :
+                    System.out.println("Goodbye.");
+                    scanner.close();
+                    System.exit(0);
+                default:
+                    System.out.println("Invalid choice.");
+            }
         }
-        
-        
-        
-
-
-
-}}
-
-
+    }
+}
