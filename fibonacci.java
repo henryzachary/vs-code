41316adfbce73a146cffd4f4c235dc3a1754bfff
henryzachary <149612405+henryzachary@users.noreply.github.com>
2024-02-13 16:05:26 -0500

    fibonacci.java

--- fibonacci.java
+++ fibonacci.java
@@ -1,21 +1,19 @@
-import java.util.Scanner;
-
+import java.util.Scanner; 
 public class Fibonacci {
     public static void main(String[] args) {
-        int N = userNum; // Number of Fibonacci terms to compute
-        int num1 = 0;
-        int num2 = 1;
+        int userNum;  
+        int num1 = 0; 
+        int num2 = 1; 
 
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); 
 
-        System.out.println("input number: ");
-        userNum = in.nextInt();
+        System.out.print("input number: ");
+        userNum = in.nextInt(); 
 
-        System.out.println("Fibonacci series up to " + N + ": ");
-        for (int i = 0; i < N; i++) {
-            System.out.print(num1 + " ");
-            int num3 = num1 + num2;
-            num1 = num2;
+        for (int i = 0; i < userNum; i++) { 
+            System.out.print(num1 + " "); 
+            int num3 = num1 + num2; 
+            num1 = num2; 
             num2 = num3;
         }
     }
